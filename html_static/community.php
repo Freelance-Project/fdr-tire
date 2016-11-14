c95b05850ed2d2468c9c90a8f310bfa6da64b778
febriansyah <febriansyah.7@gmail.com>
2016-11-14 23:23:48 +0700

    naekin lagi

---
+++ html_static/community.php
@@ -0,0 +1,185 @@
+<?php $page = ""; ?>
+<?php include('inc_header.php');?>
+<!-- middle -->
+<div id="middle-content" class="community-page">
+	<section id="news-page" class="edge-left">
+		<div class="edge-left-content">
+			<div class="photo-page-section">
+				<div class="left-capt1">
+					<div class="caption1">
+						<div class="title-cap">
+							<h3 class="whiteFont">Community</h3>
+						</div>
+					</div><!--end.caption1-->
+				</div><!--end.left-capt1-->
+				<div class="photo-banner">
+					<img src="images/content/community-banner.jpg">
+				</div>
+			</div><!--end.product-section-->
+		</div>
+	</section>
+	<section id="list-news-page">
+		<div class="wrapper">
+			<div class="left-content">
+				<div class="top-menu-product">
+					<div class="left">
+						<div class="title-edge">
+							<h3>FDR NEWS</h3>
+						</div>
+					</div>
+					<div class="right">
+						<div class="breadcum-product">
+							<ol class="breadcrumb">
+				              <li><a href="#">Home</a></li>
+				              <li><a href="#">News & Event</a></li>
+				              <li class="active">FDR News</li>
+				            </ol>
+						</div>
+					</div><!--end.right-->
+				</div><!--end.top-menu-->
+				<div class="list-news">
+					<div class="row-news">
+						<a href="#">
+							<div class="imageframe">
+								<div class="imageThumb"><img src="images/content/news-row1.jpg" /></div>
+								<div class="frameThumb"><img src="images/material/frame-white.png" /></div>
+							</div>
+						</a>
+						<div class="caption-list-news">
+							<a href="#"><h3 class="title-list-news">FDR raih MotorPlus Reader Choice Award 3 kali berturut-turut</h3></a>
+							<span class="date">24 Oktober 2016</span>
+							<p>Tabloid Motor Plus kembali menggelar Motor Plus Reader Choice Award 2016 dan ban FDR keluar sebagai juara untuk kategori ban.</p>
+							<a class="readMore" href="#">Read More</a>
+						</div>
+					</div><!--end.row-news-->
+					<div class="row-news">
+						<a href="#">
+							<div class="imageframe">
+								<div class="imageThumb"><img src="images/content/news-row2.jpg" /></div>
+								<div class="frameThumb"><img src="images/material/frame-white.png" /></div>
+							</div>
+						</a>
+						<div class="caption-list-news">
+							<a href="#"><h3 class="title-list-news">FDR raih MotorPlus Reader Choice Award 3 kali berturut-turut</h3></a>
+							<span class="date">24 Oktober 2016</span>
+							<p>Tabloid Motor Plus kembali menggelar Motor Plus Reader Choice Award 2016 dan ban FDR keluar sebagai juara untuk kategori ban.</p>
+							<a class="readMore" href="#">Read More</a>
+						</div>
+					</div><!--end.row-news-->
+					<div class="row-news">
+						<a href="#">
+							<div class="imageframe">
+								<div class="imageThumb"><img src="images/content/news-row3.jpg" /></div>
+								<div class="frameThumb"><img src="images/material/frame-white.png" /></div>
+							</div>
+						</a>
+						<div class="caption-list-news">
+							<a href="#"><h3 class="title-list-news">FDR raih MotorPlus Reader Choice Award 3 kali berturut-turut</h3></a>
+							<span class="date">24 Oktober 2016</span>
+							<p>Tabloid Motor Plus kembali menggelar Motor Plus Reader Choice Award 2016 dan ban FDR keluar sebagai juara untuk kategori ban.</p>
+							<a class="readMore" href="#">Read More</a>
+						</div>
+					</div><!--end.row-news-->
+
+					<div class="row-news">
+						<a href="#">
+							<div class="imageframe">
+								<div class="imageThumb"><img src="images/content/news-row1.jpg" /></div>
+								<div class="frameThumb"><img src="images/material/frame-white.png" /></div>
+							</div>
+						</a>
+						<div class="caption-list-news">
+							<a href="#"><h3 class="title-list-news">FDR raih MotorPlus Reader Choice Award 3 kali berturut-turut</h3></a>
+							<span class="date">24 Oktober 2016</span>
+							<p>Tabloid Motor Plus kembali menggelar Motor Plus Reader Choice Award 2016 dan ban FDR keluar sebagai juara untuk kategori ban.</p>
+							<a class="readMore" href="#">Read More</a>
+						</div>
+					</div><!--end.row-news-->
+					<div class="row-news">
+						<a href="#">
+							<div class="imageframe">
+								<div class="imageThumb"><img src="images/content/news-row2.jpg" /></div>
+								<div class="frameThumb"><img src="images/material/frame-white.png" /></div>
+							</div>
+						</a>
+						<div class="caption-list-news">
+							<a href="#"><h3 class="title-list-news">FDR raih MotorPlus Reader Choice Award 3 kali berturut-turut</h3></a>
+							<span class="date">24 Oktober 2016</span>
+							<p>Tabloid Motor Plus kembali menggelar Motor Plus Reader Choice Award 2016 dan ban FDR keluar sebagai juara untuk kategori ban.</p>
+							<a class="readMore" href="#">Read More</a>
+						</div>
+					</div><!--end.row-news-->
+					<div class="row-news">
+						<a href="#">
+							<div class="imageframe">
+								<div class="imageThumb"><img src="images/content/news-row3.jpg" /></div>
+								<div class="frameThumb"><img src="images/material/frame-white.png" /></div>
+							</div>
+						</a>
+						<div class="caption-list-news">
+							<a href="#"><h3 class="title-list-news">FDR raih MotorPlus Reader Choice Award 3 kali berturut-turut</h3></a>
+							<span class="date">24 Oktober 2016</span>
+							<p>Tabloid Motor Plus kembali menggelar Motor Plus Reader Choice Award 2016 dan ban FDR keluar sebagai juara untuk kategori ban.</p>
+							<a class="readMore" href="#">Read More</a>
+						</div>
+					</div><!--end.row-news-->
+				</div><!--end.list-news-->
+			</div><!--end.left-content-->
+			<div class="right-content">
+				<div class="right-row">
+					<img src="images/content/fdr-right.png" />
+				</div>
+				<div class="right-row">
+					<div class="top-menu-product">
+						<div class="left">
+							<div class="title-edge" style="margin-bottom:0;">
+								<h3>Trending</h3>
+							</div>
+						</div>
+					</div><!--end.top-menu-->
+					<div class="row-trending">
+						<div class="caption-list-news">
+							<a href="#"><h3 class="title-list-news">FDR Ban Pilihan PON XIX</h3></a>
+							<span class="date">24 Oktober 2016</span>
+						</div>
+						<a href="#">
+							<div class="imageframe">
+								<div class="imageThumb"><img src="images/content/news-row3.jpg" /></div>
+								<div class="frameThumb"><img src="images/material/frame-grey.png" /></div>
+							</div>
+						</a>
+					</div><!--end.row-trending-->
+
+					<div class="row-trending">
+						<div class="caption-list-news">
+							<a href="#"><h3 class="title-list-news">FDR Ban Pilihan PON XIX</h3></a>
+							<span class="date">24 Oktober 2016</span>
+						</div>
+						<a href="#">
+							<div class="imageframe">
+								<div class="imageThumb"><img src="images/content/news-row2.jpg" /></div>
+								<div class="frameThumb"><img src="images/material/frame-grey.png" /></div>
+							</div>
+						</a>
+					</div><!--end.row-trending-->
+
+					<div class="row-trending">
+						<div class="caption-list-news">
+							<a href="#"><h3 class="title-list-news">FDR Ban Pilihan PON XIX</h3></a>
+							<span class="date">24 Oktober 2016</span>
+						</div>
+						<a href="#">
+							<div class="imageframe">
+								<div class="imageThumb"><img src="images/content/news-row1.jpg" /></div>
+								<div class="frameThumb"><img src="images/material/frame-grey.png" /></div>
+							</div>
+						</a>
+					</div><!--end.row-trending-->
+				</div>
+			</div><!--end.right-content-->
+		</div>
+	</section>
+</div>
+<!-- end of middle -->
+
+<?php include('inc_footer.php');?>
---
+++ html_static/gallery-photo.php
@@ -0,0 +1,122 @@
+<?php $page = ""; ?>
+<?php include('inc_header.php');?>
+<!-- middle -->
+<div id="middle-content">
+
+	<section id="news-page" class="edge-left" style="margin-bottom:200px;">
+		<div class="edge-left-content">
+			<div class="photo-page-section">
+				<div class="left-capt1">
+					<div class="caption1">
+						<div class="title-cap">
+							<h3 class="whiteFont">Photo</h3>
+						</div>
+					</div><!--end.caption1-->
+				</div><!--end.left-capt1-->
+				<div class="photo-banner">
+					<img src="images/content/photo-banner.jpg">
+				</div>
+				<div class="photo-title">PHOTO TITLE</div>
+			</div><!--end.product-section-->
+		</div>
+	</section>
+	<section id="photo-list">
+		<div class="wrapper">
+			<div class="top-menu-product">
+				<div class="right">
+					<div class="breadcum-product">
+						<ol class="breadcrumb">
+			              <li><a href="#">Home</a></li>
+			              <li><a href="#">Gallery</a></li>
+			              <li class="active">Photo All</li>
+			            </ol>
+					</div>
+				</div><!--end.right-->
+			</div><!--end.top-menu-->
+			<div class="row-photo">
+				<div class="title-edge">
+					<h3>Racing</h3>
+				</div>
+				<div class="photolist">
+					<div class="items">
+						<a href="photo-details.php">
+							<div class="images">
+								<img src="images/content/photo-list1.jpg">
+							</div><!--end.images-->
+							<div class="details">
+								<p class="title">Photo Title</p>
+								<span class="count">100 Photos</span>
+							</div>
+						</a>
+					</div><!--end.items-->
+					<div class="items">
+						<a href="photo-details.php">
+							<div class="images">
+								<img src="images/content/photo-list2.jpg">
+							</div><!--end.images-->
+							<div class="details">
+								<p class="title">Photo Title</p>
+								<span class="count">100 Photos</span>
+							</div>
+						</a>
+					</div><!--end.items-->
+					<div class="items">
+						<a href="photo-details.php">
+							<div class="images">
+								<img src="images/content/photo-list3.jpg">
+							</div><!--end.images-->
+							<div class="details">
+								<p class="title">Photo Title</p>
+								<span class="count">100 Photos</span>
+							</div>
+						</a>
+					</div><!--end.items-->
+				</div><!--end.photo-list-->
+			</div><!--end.row-photo-->
+
+			<div class="row-photo">
+				<div class="title-edge">
+					<h3>Events</h3>
+				</div>
+				<div class="photolist">
+					<div class="items">
+						<a href="photo-details.php">
+							<div class="images">
+								<img src="images/content/photo-list1.jpg">
+							</div><!--end.images-->
+							<div class="details">
+								<p class="title">Photo Title</p>
+								<span class="count">100 Photos</span>
+							</div>
+						</a>
+					</div><!--end.items-->
+					<div class="items">
+						<a href="photo-details.php">
+							<div class="images">
+								<img src="images/content/photo-list2.jpg">
+							</div><!--end.images-->
+							<div class="details">
+								<p class="title">Photo Title</p>
+								<span class="count">100 Photos</span>
+							</div>
+						</a>
+					</div><!--end.items-->
+					<div class="items">
+						<a href="photo-details.php">
+							<div class="images">
+								<img src="images/content/photo-list3.jpg">
+							</div><!--end.images-->
+							<div class="details">
+								<p class="title">Photo Title</p>
+								<span class="count">100 Photos</span>
+							</div>
+						</a>
+					</div><!--end.items-->
+				</div><!--end.photo-list-->
+			</div><!--end.row-photo-->
+		</div>
+	</section>
+</div>
+<!-- end of middle -->
+
+<?php include('inc_footer.php');?>
