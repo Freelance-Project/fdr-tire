c719ee00e50b94b2362608d18e48050fbbc517b5
febriansyah <febriansyah.7@gmail.com>
2016-11-16 00:36:16 +0700

    template community

---
+++ html_static/community03.php
@@ -0,0 +1,117 @@
+<?php $page = ""; ?>
+<?php include('inc_header.php');?>
+<!-- middle -->
+<div id="middle-content" class="community-page">
+	<section id="news-page" class="edge-left">
+		<div class="edge-left-content">
+			<div class="photo-page-section">
+				<div class="left-capt1">
+					<div class="caption1">
+						<div class="title-cap">
+							<h3 class="whiteFont">Community</h3>
+						</div>
+					</div><!--end.caption1-->
+				</div><!--end.left-capt1-->
+				<div class="photo-banner">
+					<img src="images/content/community-banner.jpg">
+				</div>
+			</div><!--end.product-section-->
+		</div>
+	</section>
+	<section id="list-news-page">
+		<div class="wrap-community">
+			<div class="left-content">
+				<div class="top-menu-product">
+					<div class="right">
+						<div class="breadcum-product">
+							<ol class="breadcrumb">
+				              <li><a href="#">Home</a></li>
+				              <li class="active">Manage Club</li>
+				            </ol>
+						</div>
+					</div><!--end.right-->
+				</div><!--end.top-menu-->
+				<a href="#" class="edt-inf"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit Information</a>
+				<div class="table-daftar">
+					<table>
+						<tr>
+							<td>Name</td>
+							<td><input type="text"></td>
+						</tr>
+						<tr>
+							<td>Logo</td>
+							<td><img src="images/content/club-dummy.jpg" /></td>
+						</tr>
+						<tr>
+							<td>Email</td>
+							<td><input type="text"></td>
+						</tr>
+						<tr>
+							<td>Address</td>
+							<td><input type="text"></td>
+						</tr>
+						<tr>
+							<td>Contact</td>
+							<td><input type="text"></td>
+						</tr>
+						<tr>
+							<td>Description</td>
+							<td><input type="text"></td>
+						</tr>
+						<tr>
+							<td>Website</td>
+							<td><input type="text"></td>
+						</tr>
+						<tr>
+							<td>Facebook</td>
+							<td><input type="text"></td>
+						</tr>
+						<tr>
+							<td>Twitter</td>
+							<td><input type="text"></td>
+						</tr>
+						<tr>
+							<td>Milis</td>
+							<td><input type="text"></td>
+						</tr>
+						<tr>
+							<td>Other Links 1</td>
+							<td><input type="text"></td>
+						</tr>
+						<tr>
+							<td>Other Links 2</td>
+							<td><input type="text"></td>
+						</tr>
+						<tr>
+							<td>Other Links 1</td>
+							<td><input type="text"></td>
+						</tr>
+						<tr>
+							<td>Est. Date</td>
+							<td><input type="text"></td>
+						</tr>
+						<tr>
+							<td>Join Date</td>
+							<td><input type="text"></td>
+						</tr>
+					</table>
+				</div><!--end.table-daftar-->
+				
+			</div><!--end.left-content-->
+			<div class="right-content">
+				<div class="right-row">
+					<div class="menu-right">
+						<p class="greeting">Welcome,Motor Club</p>
+						<a href="#" class="menu-rounded">Manage CLub</a>
+						<a href="#" class="menu-rounded">Manage CLub News / Event</a>
+						<a href="#" class="menu-rounded"><i class="fa fa-power-off" aria-hidden="true"></i> Logout</a>
+					</div>
+				</div>
+				
+			</div><!--end.right-content-->
+		</div>
+	</section>
+</div>
+<!-- end of middle -->
+
+<?php include('inc_footer.php');?>
---
+++ html_static/community04.php
@@ -0,0 +1,70 @@
+<?php $page = ""; ?>
+<?php include('inc_header.php');?>
+<!-- middle -->
+<div id="middle-content" class="community-page">
+	<section id="news-page" class="edge-left">
+		<div class="edge-left-content">
+			<div class="photo-page-section">
+				<div class="left-capt1">
+					<div class="caption1">
+						<div class="title-cap">
+							<h3 class="whiteFont">Community</h3>
+						</div>
+					</div><!--end.caption1-->
+				</div><!--end.left-capt1-->
+				<div class="photo-banner">
+					<img src="images/content/community-banner.jpg">
+				</div>
+			</div><!--end.product-section-->
+		</div>
+	</section>
+	<section id="list-news-page">
+		<div class="wrap-community">
+			<div class="left-content">
+				<div class="top-menu-product">
+					<div class="right">
+						<div class="breadcum-product">
+							<ol class="breadcrumb">
+				              <li><a href="#">Home</a></li>
+				              <li class="active">Manage Club Contents</li>
+				            </ol>
+						</div>
+					</div><!--end.right-->
+				</div><!--end.top-menu-->
+				<div class="manage-section">
+					<div class="rounded-grey">
+						<span class="left">Manage Content</span>
+						<a href="#" class="right">+ add content</a>
+					</div>
+					<div class="select-community">
+						<div class="select-style">
+							  <select>
+							    <option value="community1">List All</option>
+							    <option value="community2">Short By Newest 2</option>
+							    <option value="community3">Short By Newest 3</option>
+							    <option value="community4">Short By Newest 4</option>
+							  </select>
+							</div><!--end.select-style-->
+						</div>
+					</div>
+					<p class="redFont no-data">No data</p>
+				</div><!--end.manage-secrtion-->
+				
+			</div><!--end.left-content-->
+			<div class="right-content">
+				<div class="right-row">
+					<div class="menu-right">
+						<p class="greeting">Welcome,Motor Club</p>
+						<a href="#" class="menu-rounded">Manage CLub</a>
+						<a href="#" class="menu-rounded">Manage CLub News / Event</a>
+						<a href="#" class="menu-rounded"><i class="fa fa-power-off" aria-hidden="true"></i> Logout</a>
+					</div>
+					
+				
+			</div><!--end.right-content-->
+		</div>
+	</section>
+</div>
+<!-- end of middle -->
+
+<?php include('inc_footer.php');?>
